6db449a3a943b04fffe04defd6a1761b983f8913
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2025-01-28 11:35:49 +0500

    66 masala qo'shildi

---
+++ 66masala.py
@@ -0,0 +1,19 @@
+#
+# def plusOne(self, digits):
+#     digits=[1,2,3]
+#     result = int("".join(str, digits))
+#     print(result)
+#
+# plusOne([1,2,3])
+
+
+
+# print(disits1)
+
+
+digits=[1,2,9]
+result =int("".join(map(str, digits)))
+result +=1
+digits1=[int(kiritish) for kiritish in str(result)]
+print(digits1)
+
